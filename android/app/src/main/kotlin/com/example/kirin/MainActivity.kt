efee9c2ff2bd7624a5e5e34f7c0c84d029103446
Roberto Jimenez Chaves <roberto.jimenez@baccredomatic.com>
2025-06-17 16:37:28 -0600

    Proyecto Kirin

---
+++ android/app/src/main/kotlin/com/example/kirin/MainActivity.kt
@@ -0,0 +1,5 @@
+package com.example.kirin
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity: FlutterActivity()
---
+++ lib/main.dart
@@ -0,0 +1,31 @@
+import 'package:flutter/material.dart';
+
+void main() {
+  runApp(const MyApp());
+}
+
+class MyApp extends StatelessWidget {
+  const MyApp({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      title: 'Kirin',
+      home: Scaffold(
+        appBar: AppBar(
+          title: const Text('Kirin'),
+        ),
+        body: const Center(
+          child: Column(
+            mainAxisAlignment: MainAxisAlignment.center,
+            children: <Widget>[
+              Text(
+                'Welcome to Kirin!',
+              ),
+            ],
+          ),
+        ),
+      ),
+    );
+  }
+}
