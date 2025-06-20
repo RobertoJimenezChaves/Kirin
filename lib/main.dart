ab0fb86aa29b5613fe86f4fed8c802fd38596ca6
Roberto Jimenez Chaves <rjc892@gmail.com>
2025-06-20 09:17:19 -0600

    devops 001: Se agregar codeowners

--- lib/main.dart
+++ lib/main.dart
@@ -10,7 +10,7 @@ class MyApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
-      title: 'Kirin',
+      title: 'Kirin App',
       home: Scaffold(
         appBar: AppBar(
           title: const Text('Kirin'),
